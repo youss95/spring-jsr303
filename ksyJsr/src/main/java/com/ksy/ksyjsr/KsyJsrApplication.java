4c28e8a08efb6d18ce32f07f3e94ef313240c75f
youss95 <usekim7555@gmail.com>
2024-01-24 21:26:40 +0900

    init

---
+++ ksyJsr/src/main/java/com/ksy/ksyjsr/KsyJsrApplication.java
@@ -0,0 +1,13 @@
+package com.ksy.ksyjsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsyJsrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KsyJsrApplication.class, args);
+    }
+
+}
---
+++ ksyJsr/src/test/java/com/ksy/ksyjsr/KsyJsrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksy.ksyjsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsyJsrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
